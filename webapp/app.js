5818e59f6108c0c604746c77fd0d6ff8d75a5318
Wei Yi Lim <lwy_1011@hotmail.com>
2022-01-24 14:54:25 +0000

    Execute algorithm.py from express.js
     - Add Procfile and requirements.txt to install python modules
     - Merge components, views, router codes into app.js
     - Add start on scripts tag in package.json to run the server
     - Add heroku-postbuild on scripts tag in package.json to build the
       react application
    
    Resolve #1

--- webapp/app.js
+++ webapp/app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const path = require('path');
 app = express()
-
 require('dotenv').config()
 
-app.set('view engine', 'ejs')
-app.use('/api/', require('./routes/graph'))
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/', (req, res) => {
+  const { spawn } = require('child_process');
+  const pythonSpawn = spawn('python', ['algorithm.py']);
+
+  pythonSpawn.stdout.on('data', (data) => {
+      res.json(JSON.parse(data));
+  });
+
+  pythonSpawn.stderr.on('data', (data) => {
+    console.log(`error:${data}`);
+ });
+
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 const PORT = process.env.PORT || 4896
 app.listen(PORT, () => {
   console.log(`Listening Port : ${PORT}`)
 });
+
